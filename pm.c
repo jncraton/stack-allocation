1ee87d5e141b68d893561f6796efffb1ddb8883f
Jon Craton <jncraton@gmail.com>
2021-02-15 16:31:23 -0500

    Add pm.c

---
+++ pm.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int pins[2] = {1234, 4729};
+const char* passwords[2] = {"Password1", "TopSecret"};
+
+void show_password() {
+  char entered_pin[8];
+  char user[8];
+
+  printf("Enter user id:");
+  fgets(user, sizeof(user) * 8, stdin);
+
+  int expected_pin = pins[atoi(user)];
+
+  printf("Enter pin:");
+
+  fgets(entered_pin, sizeof(entered_pin) * 8, stdin);
+
+  if (expected_pin == atoi(entered_pin)) {
+    printf("Pin accepted. Your stored password is %s\n", passwords[atoi(user)]);
+  } else {
+    printf("Invalid pin for user %s", user);
+  }
+}
+
+int main() {
+  show_password();
+}
